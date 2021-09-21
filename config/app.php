3726e35a0cc7a5486da1871e2f6e2d9f90e94bdb
K.Andonov <dev.andonov@gmail.com>
2021-09-21 19:11:16 +0300

    Add global environment variables.

--- config/app.php
+++ config/app.php
@@ -3,6 +3,7 @@
 
 return [
     'providers' => [
+        \App\Providers\EnvironmentVariablesServiceProvider::class,
         \App\Providers\RouteServiceProvider::class,
         \App\Providers\ErrorMiddlewareServiceProvider::class,
     ]
