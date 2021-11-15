bd5816f00ed83af2261dab7ab5dc3e145282320c
K.Andonov <dev.andonov@gmail.com>
2021-11-15 17:35:42 +0200

    Remove unused middlewares.

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,16 @@
 <?php
 
 use App\Support\Route;
-use App\Http\Middleware\RedirectIfAuthenticatedMiddleware as RedirectIfAuthenticated;
-use App\Http\Middleware\RedirectIfGuestMiddleware as RedirectIfGuest;
 
 Route::get('/', 'WelcomeController@index');
-Route::get('/report', 'WelcomeController@report')->add(RedirectIfGuest::class);
+Route::get('/report', 'WelcomeController@report');
 Route::get('/welcome/{name}', 'WelcomeController@show');
 
 Route::get('/login', 'LoginController@show');
 Route::post('/login', 'LoginController@store');
-Route::get('/logout', 'LoginController@logout')->add(RedirectIfGuest::class);
+Route::get('/logout', 'LoginController@logout');
 
-Route::get('/home', 'DashboardController@show')->add(RedirectIfGuest::class);
+Route::get('/home', 'DashboardController@show');
 
 Route::get('/register', 'RegisterController@show');
 Route::post('/register', 'RegisterController@store');
